c7ee6584dc3b8fb5fdb48b7e9cd46bdfef7dc04a
Mackenzie Bruns <mackenziebruns503@gmail.com>
2020-05-29 18:38:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,14 +54,4 @@ var empty=[]
  while(incrementVariable()<15);
  return empty
 }
-let i = 0;
- 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
 
-function doWhileLoop(num){
-  do { console.log("I run once regardless.")}
-  while (incrementVariable()<= num);
-}
